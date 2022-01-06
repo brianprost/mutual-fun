82c2140c55753be322e6f8f338eecf7d6ddbe163
Brian Prost <brian.s.prost@gmail.com>
2022-01-06 13:27:50 -0500

    updated design

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import MutualFunLogo from "../public/img/mutual-fun-logo.svg";
+import Image from "next/image";
 import Head from "next/head";
 import { Nouns, Adjectives } from "../data/words";
 
@@ -16,45 +18,31 @@ export default function Home() {
   const [companyName, setCompanyName] = useState(generateCompanyName());
 
   return (
-    <div>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Belleza&family=Inter:wght@659&display=optional"
-        />
-        <meta
-          name="theme-color"
-          content="#ddc385"
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          name="theme-color"
-          content="#003049"
-          media="(prefers-color-scheme: dark)"
-        />
-        <meta property="og:title" content="Mutual FUN" />
-        <meta property="og:image" content="img/mutual-fun-logo.jpg" />
-      </Head>
-      <div className="flex flex-col justify-center items-center h-screen bg-sky-900">
-        <img
-          src="./img/mutual-fun-logo.jpg"
-          className="sm:max-h-60 w-auto pb-20 rounded-lg"
-          alt="mutual fun logo"
-        />
-        <div className="flex items-center px-6 py-32 h-1/3 w-auto max-w-lg bg-neutral-200 rounded-lg border border-gray-200 drop-shadow-lg mx-4 md:mx-0 bg-opacity-20">
-          <h1 className="text-7xl text-center font-bold capitalize text-yellow-600 font-belleza drop-shadow-md">
+    <div className="flex flex-col justify-between h-screen bg-mf-blue">
+      <nav className="flex align-top justify-center h-28 bg-mf-gold overflow-hidden">
+        <img src="./img/mutual-fun-logo.svg" />
+      </nav>
+      <main className="flex flex-col justify-center items-center">
+        <h2 className="mb-10 text-2xl text-center text-mf-gold font-belleza ">
+          Your business name is:
+        </h2>
+        <div className="container flex flex-col justify-center h-72 mx-4 bg-neutral-200 rounded-lg border border-gray-200 drop-shadow-lg  bg-opacity-10">
+          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center font-bold capitalize text-mf-gold font-belleza drop-shadow-md">
             {companyName}
           </h1>
         </div>
-
         <button
           type="reset"
-          className="text-white bg-transparent border-2 border-white hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300  rounded-lg text-xl mt-20 px-5 py-2.5 text-center mr-2 mb-2 font-inter font-light tracking-narrow"
+          className="text-mf-gold bg-transparent border-2 border-mf-gold hover:bg-mf-gold hover:text-mf-blue hover:scale-125 rounded-lg text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-20 p-5 py-2.5 text-center font-belleza tracking-tight"
           onClick={() => setCompanyName(generateCompanyName())}
         >
           AnotherOne
         </button>
-      </div>
+        
+      </main>
+      <footer className="flex">
+      <div className="text-mf-gold text-xl font-bold font-belleza tracking-wider mb-3 ml-3">Made with ❤️ and ☕️ by Brian</div>
+      </footer>
     </div>
   );
 }
