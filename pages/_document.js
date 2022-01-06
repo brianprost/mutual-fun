1abf63f9d76d1cb87695f831758e46b1ec562c14
Brian Prost <brian.s.prost@gmail.com>
2022-01-06 13:06:40 -0500

    converted to webp and updated manifest

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,16 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default () => {
   return (
     <Html>
       <Head />
       <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Belleza&display=optional"
-        />
-        <meta
-          name="theme-color"
-          content="#ddc385"
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          name="theme-color"
-          content="#003049"
-          media="(prefers-color-scheme: dark)"
-        />
-        <meta property="og:title" content="Mutual FUN" />
-        <meta property="og:image" content="img/mutual-fun-logo.jpg" />
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Belleza&display=optional"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <meta property="og:title" content="Mutual FUN" />
+      <meta property="og:image" content="img/mutual-fun-logo.webp" />
       <body>
         <Main />
         <NextScript />
