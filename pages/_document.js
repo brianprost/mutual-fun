59ee8e8d4c1614768a6fbd6de96d2b2913a1d5f7
Brian Prost <brian.s.prost@gmail.com>
2022-01-06 14:21:50 -0500

    styling edits & ESlint changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default () => {
+const MyDocument = () => {
   return (
     <Html lang="en">
       <Head />
@@ -9,17 +9,28 @@ export default () => {
         href="https://fonts.googleapis.com/css2?family=Belleza&display=optional"
       />
       <link rel="manifest" href="/manifest.json" />
-      <meta rel="description" content="A random company name generator for a shark-tank like game."/>
+      <meta
+        rel="description"
+        content="A random company name generator for a shark-tank like game."
+      />
       <meta property="og:title" content="Mutual FUN" />
-      <meta property="og:image" content="img/mutual-fun-logo.webp" alt="mutual fun logo" />
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Mutual Fun</title>
+      <meta
+        property="og:image"
+        content="img/mutual-fun-logo.webp"
+        alt="mutual fun logo"
+      />
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Mutual Fun</title>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-};
+}
+
+MyDocument.displayName = "MyDocument";
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Image from "next/image";
 import { Nouns, Adjectives } from "../data/words";
 
 export default function Home() {
@@ -18,10 +17,10 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-between h-screen bg-mf-blue">
       <nav className="flex align-top justify-center h-28 bg-mf-gold overflow-hidden">
-        <img src="./img/mutual-fun-logo.svg" alt="mutual fun logo" />
+        <img src="/img/mutual-fun-logo.svg" alt="mutual fun logo"/>
       </nav>
       <main className="flex flex-col justify-center items-center">
-        <h2 className="mb-10 text-2xl text-center text-mf-gold font-belleza ">
+        <h2 className="mb-10 text-2xl text-center text-mf-gold font-belleza tracking-wide">
           Your business name is:
         </h2>
         <div className="container flex flex-col justify-center h-72 max-w-xs md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl bg-neutral-200 rounded-lg border border-gray-200 drop-shadow-lg bg-opacity-10">
@@ -31,14 +30,14 @@ export default function Home() {
         </div>
         <button
           type="reset"
-          className="text-mf-gold bg-transparent border-2 border-mf-gold hover:bg-mf-gold hover:text-mf-blue hover:scale-125 rounded-lg text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-20 p-5 py-2.5 text-center font-belleza tracking-tight"
+          className="text-mf-gold bg-transparent border-2 border-mf-gold hover:bg-mf-gold hover:text-mf-blue hover:scale-110 rounded-lg text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-20 p-5 py-2.5 text-center font-belleza tracking-tight"
           onClick={() => setCompanyName(generateCompanyName())}
         >
           AnotherOne
         </button>
       </main>
       <footer className="flex">
-        <div className="text-mf-gold text-xl font-bold font-belleza tracking-wider mb-3 ml-3">
+        <div className="text-mf-gold text-md:text-lg font-bold font-belleza tracking-wider mb-3 ml-3">
           Made with ❤️ and ☕️ by{" "}
           <a href="https://brianprost.com/" target="_blank" rel="noopener noreferrer"  className="underline underline-offset-4 hover:underline-offset-2">Brian Prost</a>.
         </div>
